6a497d44584794cb977409fa3818202e9f363277
NieceyX <deniece@radfords.net>
2021-11-03 18:56:56 -0500

    added printErr function and checks for a file

--- hw_5.cpp
+++ hw_5.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+std::string req = "reqFile.txt";
+
+void printErr(std::string error) {
+    std::cout << "\nERROR:\n" << "\t" << error  << "\n" << std::endl;
+}
 
 void checkFiles() {
     //will check files against the set
@@ -7,6 +15,12 @@ void checkFiles() {
 
 int main() {
     std::cout << "Hello!\n";
+    
+    if(fs::exists(req)) {
+        std::cout << "Required file exists" << std::endl;
+    }
+    else
+        printErr("Missing file containing required files: " + req);
 
     return 0;
 }
