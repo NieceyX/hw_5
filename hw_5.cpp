c607e8b757803f9ccbdfb788951894b0824cc88f
NieceyX <deniece@radfords.net>
2021-11-05 09:39:16 -0500

    all vectors get items

--- hw_5.cpp
+++ hw_5.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 //must compile in c++20
 //  g++ -std=c++20 hw_5.cpp
@@ -21,7 +22,7 @@ void printErr(std::string error) {
 void print(std::string preCur, auto items) {
     std::cout << preCur << "\n";
     for(auto item : items)
-        std::cout << item << "\n";
+        std::cout << "  " << item << "\n";
 }
 
 void checkFiles(std::vector<std::string> r) {
@@ -33,12 +34,25 @@ void checkFiles(std::vector<std::string> r) {
     std::vector<std::string> present;
     std::vector<std::string> missing;
     std::vector<std::string> extra;
-    for(auto file : r) {
-        if(fs::exists(dir + file)) {
-            present.push_back(file);
+    std::vector<std::string> junk;
+    for(auto const& dir_entry: fs::directory_iterator{dir}){
+        std::string item = dir_entry.path().string();
+        item = item.erase(0, dir.size());
+        if(std::find(r.begin(), r.end(), item) != r.end()) {
+            present.push_back(item);
         }
+        else {
+            extra.push_back(item);
+        }
+
+    }
+    for(const auto &file : r) {
+        fs::exists(dir + file) ? junk.push_back(file) : missing.push_back(file);
     }
     print("Present files:", present);
+    print("Missing files:", missing);
+    print("Extra files:", extra);
+    
 }
 
 
